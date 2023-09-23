e42241d3ee461efff0888502ca21dde305becf90
thanhngan22 <nganbui9a2017@gmail.com>
2023-09-23 09:38:22 +0700

    add imgs demo/ fixed and rebuilt project successfully | finished task in ms word

--- Week01/CurrencyConverter/app/build.gradle.kts
+++ Week01/CurrencyConverter/app/build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     id("com.android.application")
-//    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
-
-//ext.kotlin_version = "1.8.22"
 
 android {
     namespace = "com.example.currencyconverter"
--- Week01/CurrencyConverter/app/src/main/java/com/example/currencyconverter/Currency1.java
+++ Week01/CurrencyConverter/app/src/main/java/com/example/currencyconverter/Currency1.java
@@ -78,6 +78,4 @@ public class Currency1 extends Activity {
             }
         });
     }
-
-
 }
