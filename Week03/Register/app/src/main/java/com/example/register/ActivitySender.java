d78b1b8e19c53562662c76059c2e13031ee93a2c
thanhngan22 <nganbui9a2017@gmail.com>
2023-10-03 15:30:53 +0700

    set event onClick reset and signup buttons/ store data in bundle and transfer for next activity: confirm

--- Week03/Register/app/src/main/java/com/example/register/ActivitySender.java
+++ Week03/Register/app/src/main/java/com/example/register/ActivitySender.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
@@ -27,6 +28,8 @@ public class ActivitySender extends AppCompatActivity {
     private RadioButton maleGenderInput, femaleGenderInput;
     private CheckBox tennisHobbiesInput, footballHobbiesInput, othersHobbiesInput;
     private Button selectBtn, resetBtn, signupBtn;
+    
+    private Context context;    // used as param in toast method
 
 
     @Override
@@ -54,21 +57,127 @@ public class ActivitySender extends AppCompatActivity {
         signupBtn = findViewById(R.id.resetBtn);
 
 //       1. set event onCLick select button | show datePicker dialog
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
 
-//        2. set event onClick reset button | reset all input from user with ""
+//        2. set event onClick reset button | reset all input from user with ""     *** done
+            resetBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    usernameInput.setText("");
+                    passwordInput.setText("");
+                    retypeInput.setText("");
+                    birthdateInput.setText("dd/mm/yyyy");
 
+//                    reset radio buttons and check buttons
+                    if (maleGenderInput.isChecked()) {
+                        maleGenderInput.setChecked(false);
+                    }
+
+                    if (femaleGenderInput.isChecked()) {
+                        femaleGenderInput.setChecked(false);
+                    }
+
+                    if (tennisHobbiesInput.isChecked()) {
+                        tennisHobbiesInput.setChecked(false);
+                    }
+
+                    if (footballHobbiesInput.isChecked()) {
+                        footballHobbiesInput.setChecked(false);
+                    }
+
+                    if (othersHobbiesInput.isChecked()) {
+                        othersHobbiesInput.setChecked(false);
+                    }
+                }
+            });
 
 
 //      3. set event onClick sign up button
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context = getApplicationContext();
+
+//                get inform from input and store to some variables
+                String birthdate = birthdateInput.getText().toString();
+                String gender = "", hobbies = "";
+                if (maleGenderInput.isChecked()) {
+                    gender = "male";
+                } else if (femaleGenderInput.isChecked()) {
+                    gender = "female";
+                } else {
+//                    no gender input is checked | notify ?
+                }
+
+                if (tennisHobbiesInput.isChecked()) {
+                    hobbies = "Tennis";
+                }
+
+                if (footballHobbiesInput.isChecked()) {
+                    hobbies = hobbies + ", Football";
+                }
+
+                if (othersHobbiesInput.isChecked()) {
+                    hobbies = hobbies + " and others";
+                }
+
 //          3.1 | check whether password and retype is same or not
+                if (passwordInput.getText() != retypeInput.getText()) {
 //              | notify user to enter password and retype again if they are diff (using Toast)
+                    Toast.makeText(context, "the password and retype don't match !!!", Toast.LENGTH_SHORT).show();
+                }
 //          3.2 | check whether birthdate is in format dd/mm/yyyy or not
+                if ( ! isFollowedDateFormat(birthdate) ) {
 //              | notify user to enter birthdate again if it is not in format (using Toast)
+                    Toast.makeText(context, "birthdate entered is not in format dd/mm/yyyy !!!", Toast.LENGTH_SHORT).show();
+                }
+
+                if ( ! isDateExist(birthdate) ) {
+
+                }
+
 //          3.3 | send information for ActivityReceiver to display in Confirm form
+                Intent intentToConfirm = new Intent(ActivitySender.this, ActivityReceiver.class);
+
+
+//                create bundle to store and transfer data to another activity
+                Bundle senderBundle = new Bundle();
+                senderBundle.putString("username", usernameInput.getText().toString());
+                senderBundle.putString("password", passwordInput.getText().toString());
+                senderBundle.putString("birthdate", birthdate );
+                senderBundle.putString("gender", gender);
+                senderBundle.putString("hobbies", hobbies);
+
+                intentToConfirm.putExtras(senderBundle);
+                startActivity(intentToConfirm);
+            }
+        });
+
+
 
 
     }
+
+//    function check string is in format dd/mm/yyyy
+    protected boolean isFollowedDateFormat(String date) {
+
+
+        return true;
+    }
+
+//    function check whether date entered is exist or not
+    protected boolean isDateExist(String date) {
+
+
+        return true;
+    }
+
+
 
 
 }
